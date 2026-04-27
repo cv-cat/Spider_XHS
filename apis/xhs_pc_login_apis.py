290dc4ae473a7bc00deb5273ea1b2df3722c2732
cvcat <992822653@qq.com>
2026-04-27 16:50:34 +0800

    feat: update

--- apis/xhs_pc_login_apis.py
+++ apis/xhs_pc_login_apis.py
@@ -3,10 +3,7 @@ import time
 import random
 import uuid
 
-import aiohttp
-import asyncio
 import requests
-
 import qrcode
 from loguru import logger
 
@@ -20,10 +17,7 @@ class XHSLoginApi:
         self.base_url = "https://edith.xiaohongshu.com"
         self.as_url = "https://as.xiaohongshu.com"
         self.home_url = 'https://www.xiaohongshu.com/explore'
-        self.generate_qrcode_api = '/api/sns/web/v1/login/qrcode/create'
-        self.check_qrcode_api = '/api/qrcode/userinfo'
 
-    # 生成初始cookies
     @staticmethod
     def _get_sec_headers():
         return {
@@ -110,105 +104,46 @@ class XHSLoginApi:
 
         return cookies
 
-    # 手机验证码登录
-    async def send_phone_code(self, phone_num, cookies):
-        try:
-            api = "/api/sns/web/v2/login/send_code"
-            params = {
-                "phone": phone_num,
-                "zone": "86",
-                "type": "login"
-            }
-            splice_api = splice_str(api, params)
-            headers, _ = generate_headers(cookies['a1'], splice_api)
-            async with aiohttp.ClientSession() as session:
-                async with session.get(self.base_url + splice_api, headers=headers, cookies=cookies) as response:
-                    res = await response.json()
-                    logger.debug(res)
-                    success, msg = res['success'], res['msg']
-        except Exception as e:
-            success, msg = False, str(e)
-        return success, msg
-
-    async def check_phone_code(self, phone_num, code, cookies):
-        mobile_token = None
-        try:
-            api = "/api/sns/web/v1/login/check_code"
-            params = {
-                "phone": phone_num,
-                "zone": "86",
-                "code": code
-            }
-            splice_api = splice_str(api, params)
-            headers, _ = generate_headers(cookies['a1'], splice_api)
-            async with aiohttp.ClientSession() as session:
-                async with session.get(self.base_url + splice_api, headers=headers, cookies=cookies) as response:
-                    res = await response.json()
-                    success, msg = res['success'], res['msg']
-                    mobile_token = res['data']['mobile_token']
-        except Exception as e:
-            success, msg = False, str(e)
-        return success, msg, mobile_token
-
-    async def login_by_phone(self, mobile_token, phone, cookies):
-        cookies_str = None
-        try:
-            api = "/api/sns/web/v2/login/code"
-            data = {
-                "mobile_token": mobile_token,
-                "zone": "86",
-                "phone": phone
-            }
-            headers, data = generate_headers(cookies['a1'], api, data)
-            async with aiohttp.ClientSession() as session:
-                async with session.post(self.base_url + api, headers=headers, cookies=cookies, data=data) as response:
-                    res = await response.json()
-                    success, msg = res['success'], res['msg']
-                    cookies['web_session'] = res['data']['session']
-                    cookies_str = '; '.join(f'{k}={v}' for k, v in cookies.items())
-        except Exception as e:
-            success, msg = False, str(e)
-        return success, msg, cookies_str
-
-    # 二维码扫描登录
     def generate_qrcode(self, cookies):
-        try:
-            data = {"qr_type": 1}
-            headers, data = generate_headers(cookies['a1'], self.generate_qrcode_api, data)
-            response = requests.post(
-                self.base_url + self.generate_qrcode_api,
-                headers=headers, cookies=cookies, data=data
-            )
-            res = response.json()
-            qr_id, code, verify_url = res['data']['qr_id'], res['data']['code'], res["data"]["url"]
-            success, msg = res['success'], res['msg']
-        except Exception as e:
-            return False, str(e), None
-        return success, msg, {
-            "cookies": cookies,
-            "qr_id": qr_id,
-            "code": code,
-            "verify_url": verify_url
+        api = '/api/sns/web/v1/login/qrcode/create'
+        data = {"qr_type": 1}
+
+        headers, data = generate_headers(cookies['a1'], api, data)
+        resp = requests.post(
+            self.base_url + api,
+            headers=headers, cookies=cookies, data=data
+        )
+        for key, value in resp.cookies.items():
+            cookies[key] = value
+
+        res = resp.json()
+        if not res.get('success'):
+            return False, res.get('msg', '未知错误'), None
+
+        return True, '成功', {
+            'cookies': cookies,
+            'qr_id': res['data']['qr_id'],
+            'code': res['data']['code'],
+            'qr_url': res['data']['url'],
         }
 
     def check_qrcode_status(self, qr_id, code, cookies):
-        code_status = -1
-        try:
-            data = {"qrId": qr_id, "code": code}
-            headers, data = generate_headers(cookies['a1'], self.check_qrcode_api, data)
-            response = requests.post(
-                self.base_url + self.check_qrcode_api,
-                headers=headers, cookies=cookies, data=data
-            )
-            res = response.json()
-            code_status = res['data']['codeStatus']
-            if code_status == 2:
-                if 'web_session' in response.cookies:
-                    cookies['web_session'] = response.cookies['web_session']
-                elif 'loginInfo' in res.get('data', {}):
-                    cookies['web_session'] = res['data']['loginInfo']['session']
-        except Exception as e:
-            logger.error(f'轮询异常: {e}')
+        api = '/api/qrcode/userinfo'
+        data = {"qrId": qr_id, "code": code}
+
+        headers, data = generate_headers(cookies['a1'], api, data)
+        resp = requests.post(
+            self.base_url + api,
+            headers=headers, cookies=cookies, data=data
+        )
+        for key, value in resp.cookies.items():
+            cookies[key] = value
+
+        res = resp.json()
+        status = res['data']['codeStatus']
+
+        if status == 2:
+            cookies = self._login_by_qrcode_status(qr_id, code, cookies)
 
         status_map = {
             0: (False, '请扫描二维码'),
@@ -216,8 +151,90 @@ class XHSLoginApi:
             2: (True, '验证成功'),
             3: (False, '二维码已过期'),
         }
-        success, msg = status_map.get(code_status, (False, f'未知codeStatus: {code_status}'))
+        success, msg = status_map.get(status, (False, f'未知状态: {status}'))
         return success, msg, cookies
+
+    def _login_by_qrcode_status(self, qr_id, code, cookies):
+        api = '/api/sns/web/v1/login/qrcode/status'
+        params = {"qr_id": qr_id, "code": code}
+        splice_api = splice_str(api, params)
+
+        headers, _ = generate_headers(cookies['a1'], splice_api, method='GET')
+        resp = requests.get(
+            self.base_url + splice_api,
+            headers=headers, cookies=cookies
+        )
+        for key, value in resp.cookies.items():
+            cookies[key] = value
+
+        res = resp.json()
+        if res.get('success') and 'login_info' in res.get('data', {}):
+            login_info = res['data']['login_info']
+            if 'session' in login_info and 'web_session' not in cookies:
+                cookies['web_session'] = login_info['session']
+
+        return cookies
+
+    def get_user_info(self, cookies):
+        api = '/api/sns/web/v2/user/me'
+
+        headers, _ = generate_headers(cookies['a1'], api)
+        resp = requests.get(
+            self.base_url + api,
+            headers=headers, cookies=cookies
+        )
+        for key, value in resp.cookies.items():
+            cookies[key] = value
+
+        res = resp.json()
+        return res.get('success', False), res.get('data', {}), cookies
+
+    def send_phone_code(self, phone, cookies, zone='86'):
+        api = '/api/sns/web/v2/login/send_code'
+        params = {"phone": phone, "zone": zone, "type": "login"}
+        splice_api = splice_str(api, params)
+
+        headers, _ = generate_headers(cookies['a1'], splice_api)
+        resp = requests.get(
+            self.base_url + splice_api,
+            headers=headers, cookies=cookies
+        )
+        res = resp.json()
+        return res.get('success', False), res.get('msg', ''), res
+
+    def login_by_phone(self, phone, code, cookies, zone='86'):
+        check_api = '/api/sns/web/v1/login/check_code'
+        params = {"phone": phone, "zone": zone, "code": code}
+        splice_api = splice_str(check_api, params)
+
+        headers, _ = generate_headers(cookies['a1'], splice_api)
+        resp = requests.get(
+            self.base_url + splice_api,
+            headers=headers, cookies=cookies
+        )
+        res = resp.json()
+        if not res.get('success'):
+            return False, res.get('msg', '验证码验证失败'), {'cookies': cookies}
+        mobile_token = res['data']['mobile_token']
+
+        login_api = '/api/sns/web/v2/login/code'
+        data = {"mobile_token": mobile_token, "zone": zone, "phone": phone}
+        headers, data = generate_headers(cookies['a1'], login_api, data)
+        resp = requests.post(
+            self.base_url + login_api,
+            headers=headers, cookies=cookies, data=data
+        )
+        for key, value in resp.cookies.items():
+            cookies[key] = value
+
+        res = resp.json()
+        if not res.get('success'):
+            return False, res.get('msg', '登录失败'), {'cookies': cookies}
+        cookies['web_session'] = res['data']['session']
+        return True, '成功', {
+            'cookies': cookies,
+            'res_json': res,
+        }
 
     @staticmethod
     def cookies_to_str(cookies):
@@ -239,11 +256,11 @@ class XHSLoginApi:
         img.show()
 
     def qrcode_login(self, show_in_terminal=True):
-        logger.info('[1/3] 正在生成初始cookies...')
+        logger.info('[1/4] 正在生成初始cookies...')
         cookies = self.generate_init_cookies()
         logger.info(f'a1={cookies["a1"]}')
 
-        logger.info('[2/3] 正在获取二维码...')
+        logger.info('[2/4] 正在获取二维码...')
         success, msg, qr_data = self.generate_qrcode(cookies)
         if not success:
             logger.error(f'获取二维码失败: {msg}')
@@ -252,11 +269,11 @@ class XHSLoginApi:
 
         logger.info('请使用小红书APP扫描以下二维码:')
         if show_in_terminal:
-            self.show_qrcode_terminal(qr_data['verify_url'])
+            self.show_qrcode_terminal(qr_data['qr_url'])
         else:
-            self.show_qrcode_image(qr_data['verify_url'])
+            self.show_qrcode_image(qr_data['qr_url'])
 
-        logger.info('[3/3] 等待扫码...')
+        logger.info('[3/4] 等待扫码...')
         while True:
             success, msg, cookies = self.check_qrcode_status(
                 qr_data['qr_id'], qr_data['code'], cookies
@@ -269,39 +286,53 @@ class XHSLoginApi:
                 return None
             time.sleep(2)
 
+        logger.info('[4/4] 验证登录状态...')
+        success, user_info, cookies = self.get_user_info(cookies)
+        if success:
+            logger.info(f'用户: {user_info.get("nickname", "未知")} (RedID: {user_info.get("red_id", "未知")})')
+        else:
+            logger.warning('获取用户信息失败，但cookies可能仍有效')
+
         cookies_str = self.cookies_to_str(cookies)
         logger.success(f'登录成功!\ncookies:\n{cookies_str}')
         return cookies_str
 
-    async def phone_login(self):
+    def phone_login(self):
+        logger.info('[1/4] 正在生成初始cookies...')
         cookies = self.generate_init_cookies()
-        phone_num = input("请输入手机号：")
+        logger.info(f'a1={cookies["a1"]}')
 
-        logger.info('正在发送验证码...')
-        success, msg = await self.send_phone_code(phone_num, cookies)
+        phone = input('请输入手机号: ')
+        logger.info('[2/4] 正在发送验证码...')
+        success, msg, _ = self.send_phone_code(phone, cookies)
         if not success:
             logger.error(f'发送失败: {msg}')
             return None
         logger.info('验证码已发送')
 
-        code = input("请输入验证码：")
-        success, msg, mobile_token = await self.check_phone_code(phone_num, code, cookies)
+        code = input('请输入验证码: ')
+        logger.info('[3/4] 正在验证...')
+        success, msg, result = self.login_by_phone(phone, code, cookies)
         if not success:
             logger.error(f'验证失败: {msg}')
             return None
+        cookies = result['cookies']
 
-        success, msg, cookies_str = await self.login_by_phone(mobile_token, phone_num, cookies)
-        if not success:
-            logger.error(f'登录失败: {msg}')
-            return None
+        logger.info('[4/4] 验证登录状态...')
+        success, user_info, cookies = self.get_user_info(cookies)
+        if success:
+            logger.info(f'用户: {user_info.get("nickname", "未知")} (RedID: {user_info.get("red_id", "未知")})')
 
+        cookies_str = self.cookies_to_str(cookies)
         logger.success(f'登录成功!\ncookies:\n{cookies_str}')
         return cookies_str
 
 
 if __name__ == '__main__':
     login_api = XHSLoginApi()
-    cookies_str = login_api.qrcode_login(show_in_terminal=True)
+    # cookies_str = login_api.qrcode_login(show_in_terminal=True)
+    cookies_str = login_api.phone_login()
+
     xhs_apis = XHS_Apis()
     # 获取用户信息
     user_url = 'https://www.xiaohongshu.com/user/profile/67a332a2000000000d008358?xsec_token=ABTf9yz4cLHhTycIlksF0jOi1yIZgfcaQ6IXNNGdKJ8xg=&xsec_source=pc_feed'
